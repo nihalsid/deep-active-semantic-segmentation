766c283009c961b70a7bbca0165b388f02c5ab3b
nihalsid <yawarnihal@gmail.com>
2019-04-19 12:17:10 +0200

    Move to raid for datasets

--- constants.py
+++ constants.py
@@ -1,4 +1,4 @@
-DATASET_ROOT = './datasets/'
+DATASET_ROOT = '/mnt/raid/yawar/deeplab_pytorch/datasets/'
 RUNS = '/mnt/raid/yawar/deeplab_pytorch/runs/'
 VISUALIZATIONS_FOLDER = 'visualizations'
 TENSORBOARD_VISUALIZATION_INTERVAL = 10 # for every 10% of data
